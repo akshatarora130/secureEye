5590dbbc4a6c0fed34163d072068b10c5cccdb6b
akshatarora130 <akshatarora130@gmail.com>
2024-09-15 01:54:32 +0530

    login and home page completed

--- secure-eye-ui/src/components/tooltip.tsx
+++ secure-eye-ui/src/components/tooltip.tsx
@@ -5,30 +5,26 @@ const people = [
   {
     id: 1,
     name: "Akshat Arora",
-    designation: "Backend Engineer",
-    image:
-      "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+    designation: "Full Stack Engineer",
+    image: "https://i.ibb.co/vxZSw7P/PXL-20240716-175340590-NIGHT.jpg",
   },
   {
     id: 2,
     name: "Dhananjay Pundir",
-    designation: "Product Manager",
-    image:
-    "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+    designation: "Full Stack Engineer",
+    image: "https://i.ibb.co/2YyqKdw/IMG-1546.jpg",
   },
   {
     id: 3,
-    name: "Ishav Smith",
-    designation: "Full-Stack",
-    image:
-        "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+    name: "Ishav ",
+    designation: "Full Stack Engineer",
+    image: "https://i.ibb.co/XJs1cCp/Screenshot-2024-07-31-at-11-07-43-AM.png",
   },
   {
     id: 4,
     name: "Sparsh Gupta",
-    designation: "UX Designer",
-    image:
-        "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+    designation: "Full Stack Engineer",
+    image: "https://i.ibb.co/g6fn1Sv/Untitled-design.jpg",
   },
 ];
 
