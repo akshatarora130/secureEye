149f86b31eb3865208a30e0b9c5645142701df25
Sparsh Gupta <sparshdheeeraj@gmail.com>
2024-10-20 12:08:07 +0530

    auth fixed from ui folder

--- secure-eye-ui/src/app/api/auth/[...nextauth]/route.ts
+++ secure-eye-ui/src/app/api/auth/[...nextauth]/route.ts
@@ -1,36 +1,35 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import { AuthOptions } from "next-auth";
 
-import { db } from "../../../../../../secure-eye-Backend/src/db";
+const prisma = new PrismaClient();
 
-const prisma = db;
-
-type SessionProps = {
+interface SessionProps {
   session: any;
   user: any;
-};
+}
 
-export const authOptions = {
+export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId:
-        "280728861898-523g0lr1uat8it2sfurf2a09m7q9j0vu.apps.googleusercontent.com",
-      clientSecret: "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",
-    }),
+      "280728861898-523g0lr1uat8it2sfurf2a09m7q9j0vu.apps.googleusercontent.com",
+    clientSecret: "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",}),
   ],
-
   secret: process.env.NEXTAUTH_SECRET,
-
   callbacks: {
     async session({ session, user }: SessionProps) {
-      session.user.id = user.id;
+      if (session?.user) {
+        session.user.id = user.id;
+      }
       return session;
     },
   },
 };
 
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
